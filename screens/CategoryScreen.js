60bfd53f4ca92654fb57c99a3a22ed04093a9b3d
GP <gregpetropoulos@yahoo.com>
2023-12-30 09:24:03 -0500

    Started fresh project added data and models , created a category screen for the home page with grid child component and styles for ios and android, set global background color in app.json

---
+++ screens/CategoryScreen.js
@@ -0,0 +1,24 @@
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTitle from '../component/CategoryGridTitle';
+
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTitle
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
+};
+const CategoryScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoryScreen;
